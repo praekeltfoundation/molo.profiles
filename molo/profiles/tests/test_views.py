e4c58040096693192cbd47100763c737a3e8273f
Simon de Haan <simon@praekeltfoundation.org>
2015-08-03 20:39:41 +0200

    more tests

--- molo/profiles/tests/test_views.py
+++ molo/profiles/tests/test_views.py
@@ -1,27 +1,35 @@
 from datetime import date
 
+from django.conf.urls import patterns, url, include
 from django.contrib.auth.models import User
-from django.test import TestCase
-from django.test.client import Client
 from django.core.urlresolvers import reverse
+from django.test import TestCase, override_settings, Client
 
 from molo.profiles.forms import RegistrationForm
 
 
-class ViewTest(TestCase):
+urlpatterns = patterns(
+    '',
+    url(r'^profiles/',
+        include('molo.profiles.urls',
+                namespace='molo.profiles',
+                app_name='molo.profiles')),
+)
 
-    urls = 'molo.profiles.urls'
+
+@override_settings(ROOT_URLCONF='molo.profiles.tests.test_views')
+class ViewTest(TestCase):
 
     def setUp(self):
         self.client = Client()
 
-    def test_register_view(self):
-        response = self.client.get(reverse('user_register'))
+    def test_registeration_view(self):
+        response = self.client.get(reverse('molo.profiles:user_register'))
         self.assertTrue(isinstance(response.context['form'], RegistrationForm))
 
     def test_register_sets_dob(self):
         self.assertFalse(User.objects.filter(username='testing').exists())
-        response = self.client.post(reverse('user_register'), {
+        response = self.client.post(reverse('molo.profiles:user_register'), {
             'username': 'testing',
             'password': '1234',
             'date_of_birth': '1980-01-01',
