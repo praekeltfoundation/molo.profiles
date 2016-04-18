01f005a7ed09eb8900058b117e140aee245314a2
Saeed Marzban <Saeed.mb8@gmail.com>
2016-04-18 23:32:19 +0200

    added export user data as CSV

--- molo/profiles/admin.py
+++ molo/profiles/admin.py
@@ -1,8 +1,10 @@
+import csv
+
+from django.contrib import admin
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.admin.sites import NotRegistered
-from django.contrib import admin
-
 
 try:
     admin.site.unregister(User)
@@ -10,12 +12,30 @@ except NotRegistered:
     pass
 
 
+def download_as_csv(ProfileUserAdmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;filename=export.csv'
+    writer = csv.writer(response)
+    user_model_fields = UserAdmin.list_display + ('date_joined', )
+    profile_fields = ('alias', 'mobile_number')
+    field_names = user_model_fields + profile_fields
+    writer.writerow(field_names)
+    for obj in queryset:
+        writer.writerow(
+            [getattr(obj, field) for field in user_model_fields] +
+            [getattr(obj.profile, field) for field in profile_fields])
+    return response
+download_as_csv.short_description = "Download selected as csv"
+
+
 @admin.register(User)
 class ProfileUserAdmin(UserAdmin):
     list_display = UserAdmin.list_display + (
-        'date_joined', '_alias', '_mobile_number')
+        '_date_joined', '_alias', '_mobile_number')
 
     list_filter = UserAdmin.list_filter + ('date_joined', )
+
+    actions = [download_as_csv]
 
     def _alias(self, obj, *args, **kwargs):
         if hasattr(obj, 'profile') and obj.profile.alias:
@@ -26,3 +46,6 @@ class ProfileUserAdmin(UserAdmin):
         if hasattr(obj, 'profile') and obj.profile.mobile_number:
             return obj.profile.mobile_number
         return ''
+
+    def _date_joined(self, obj, *args, **kwargs):
+        return obj.date_joined.date()
