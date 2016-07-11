a188628ec309fc03093ade377b55fc8728de5d1f
Codie Roelf <codie@praekelt.com>
2016-07-11 13:06:50 +0200

    adding email to context processor

--- molo/profiles/context_processors.py
+++ molo/profiles/context_processors.py
@@ -6,6 +6,7 @@ def get_profile_data(request):
     alias = ''
     date_of_birth = ''
     mobile_number = ''
+    email = ''
     if request.user.is_authenticated():
         user = request.user
         profile = user.profile
@@ -16,12 +17,15 @@ def get_profile_data(request):
             alias = 'Anonymous'
         date_of_birth = profile.date_of_birth
         mobile_number = profile.mobile_number
+        email = profile.email
+        print email
     edit_profile_form = EditProfileForm(
         initial={
             'username': username,
             'alias': alias,
             'mobile_number': mobile_number,
-            'date_of_birth': date_of_birth
+            'date_of_birth': date_of_birth,
+            'email': email
         }
     )
     return {
@@ -29,5 +33,6 @@ def get_profile_data(request):
         'alias': alias,
         'date_of_birth': date_of_birth,
         'mobile_number': mobile_number,
+        'email': email,
         'edit_profile_form': edit_profile_form
     }
