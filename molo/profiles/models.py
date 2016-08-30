311193cae438dd55f53801feb90cfe396f53ddaf
phalamag <phalamag@gmail.com>
2016-08-30 22:54:53 +0200

    Added Security Questions to UserProfiles

--- molo/profiles/models.py
+++ molo/profiles/models.py
@@ -1,12 +1,14 @@
+from django.contrib.auth import hashers
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+
 from phonenumber_field.modelfields import PhoneNumberField
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.wagtailadmin.edit_handlers import (
     FieldPanel, MultiFieldPanel)
-from django.utils.translation import ugettext_lazy as _
 
 
 @register_setting
@@ -19,13 +21,13 @@ class UserProfilesSettings(BaseSetting):
     mobile_number_required = models.BooleanField(
         default=False,
         editable=True,
-        verbose_name=_('Mobile number required'),
+        verbose_name=_("Mobile number required"),
     )
 
     prevent_phone_number_in_username = models.BooleanField(
         default=False,
         editable=True,
-        verbose_name=_('Prevent phone number in username / display name'),
+        verbose_name=_("Prevent phone number in username / display name"),
     )
 
     show_email_field = models.BooleanField(
@@ -42,7 +44,18 @@ class UserProfilesSettings(BaseSetting):
     prevent_email_in_username = models.BooleanField(
         default=False,
         editable=True,
-        verbose_name=_('Prevent email in username / display name'),
+        verbose_name=_("Prevent email in username / display name"),
+    )
+
+    show_security_question_fields = models.BooleanField(
+        default=False,
+        editable=True,
+        verbose_name=_("Add security question fields to registration")
+    )
+    security_questions_required = models.BooleanField(
+        default=False,
+        editable=True,
+        verbose_name=_("Security questions required")
     )
 
     panels = [
@@ -52,17 +65,30 @@ class UserProfilesSettings(BaseSetting):
                 FieldPanel('mobile_number_required'),
                 FieldPanel('prevent_phone_number_in_username'),
             ],
-            heading="Mobile Number Settings",),
+            heading="Mobile Number Settings", ),
         MultiFieldPanel(
             [
                 FieldPanel('show_email_field'),
                 FieldPanel('email_required'),
                 FieldPanel('prevent_email_in_username'),
             ],
-            heading="Email Settings",)
+            heading="Email Settings", ),
+        MultiFieldPanel(
+            [
+                FieldPanel("show_security_question_fields"),
+                FieldPanel("security_questions_required"),
+            ],
+            heading="Security Question Settings", )
     ]
     # TODO: mobile_number_required field shouldn't be shown
     # if show_mobile_number_field is False
+
+
+class SecurityQuestion(models.Model):
+    question = models.CharField(max_length=250, null=False, blank=False)
+
+    def __str__(self):
+        return self.question
 
 
 class UserProfile(models.Model):
@@ -80,6 +106,10 @@ class UserProfile(models.Model):
         null=True)
 
     mobile_number = PhoneNumberField(blank=True, null=True, unique=False)
+    security_question_answers = models.ManyToManyField(
+        SecurityQuestion,
+        through="SecurityAnswer"
+    )
 
 
 @receiver(post_save, sender=User)
@@ -87,3 +117,28 @@ def user_profile_handler(sender, instance, created, **kwargs):
     if created:
         profile = UserProfile(user=instance)
         profile.save()
+
+
+class SecurityAnswer(models.Model):
+    user = models.ForeignKey(UserProfile)
+    question = models.ForeignKey(SecurityQuestion)
+    answer = models.CharField(max_length=150, null=False, blank=False)
+
+    def set_answer(self, raw_answer):
+        self.answer = hashers.make_password(raw_answer.strip().lower())
+
+    def check_answer(self, raw_answer):
+        def setter(raw_answer):
+            self.set_answer(raw_answer)
+            self.save(update_fields=["answer"])
+
+        return hashers.check_password(
+            raw_answer.strip().lower(),
+            self.answer,
+            setter
+        )
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.set_answer(self.answer)
+        super(SecurityAnswer, self).save(*args, **kwargs)
