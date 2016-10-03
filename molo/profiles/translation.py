3d063ef75b6f1f5e64b8c48211912bf708565a5d
Phala Ramonyai <phalamag@gmail.com>
2016-10-03 16:14:17 +0200

    Remove unused translation.py

--- molo/profiles/translation.py
+++
@@ -1,8 +0,0 @@
-from modeltranslation.translator import translator, TranslationOptions
-from molo.profiles.models import SecurityQuestion
-
-
-class SecurityQuestionTranslationOptions(TranslationOptions):
-    fields = ("question", )
-
-translator.register(SecurityQuestion, SecurityQuestionTranslationOptions)
