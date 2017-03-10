e37efdc578801b037dc08c5ca5d388d793098161
Codie Roelf <codie@praekelt.com>
2017-03-10 15:33:02 +0200

    refactoring migration

--- molo/profiles/migrations/0014_add_curent_site_to_user_profile.py
+++ molo/profiles/migrations/0014_add_curent_site_to_user_profile.py
@@ -6,20 +6,15 @@ from django.db import migrations, models
 
 def add_site_to_user_profile(apps, schema_editor):
     UserProfile = apps.get_model("profiles", "UserProfile")
-    Main = apps.get_model("core", "Main")
-    main = Main.objects.all().first()
-    if main:
-        site = main.get_site()
-        front_end_user_profiles = UserProfile.objects.filter(
-            user__is_staff=False)
-
-        for profile in front_end_user_profiles:
-            profile.site = site
-            profile.save()
+    Site = apps.get_model("wagtailcore", "Site")
+    site = Site.objects.all().first()
+    if site:
+        UserProfile.objects.filter(user__is_staff=False).update(site=site)
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('wagtailcore', '__latest__'),
         ('core', '0013_add_site_to_profile'),
     ]
 
