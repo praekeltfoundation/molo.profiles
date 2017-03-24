15efa8989883010c765a9e1dd439264a2e989655
TrevorOctober <trevor@praekeltconsulting.com>
2017-03-24 13:27:30 +0200

    update done form class and test names on RegistrationDone

--- molo/profiles/tests/test_views.py
+++ molo/profiles/tests/test_views.py
@@ -721,7 +721,7 @@ class RegistrationDone(TestCase, MoloTestCaseMixin):
         self.client.login(username='tester', password='tester')
         self.mk_main()
 
-    def test_date_of_birth(self):
+    def test_date_of_birth_on_done(self):
         site = Site.objects.get(is_default_site=True)
         profile_settings = UserProfilesSettings.for_site(site)
 
@@ -737,7 +737,7 @@ class RegistrationDone(TestCase, MoloTestCaseMixin):
         user = User.objects.get(username='tester')
         self.assertEqual(user.profile.date_of_birth, date(2000, 1, 1))
 
-    def test_display_name(self):
+    def test_display_name_on_done(self):
         site = Site.objects.get(is_default_site=True)
         profile_settings = UserProfilesSettings.for_site(site)
 
@@ -753,7 +753,7 @@ class RegistrationDone(TestCase, MoloTestCaseMixin):
         user = User.objects.get(username='tester')
         self.assertEqual(user.profile.alias, ('foo'))
 
-    def test_gender(self):
+    def test_gender_on_done(self):
         site = Site.objects.get(is_default_site=True)
         profile_settings = UserProfilesSettings.for_site(site)
 
@@ -769,7 +769,7 @@ class RegistrationDone(TestCase, MoloTestCaseMixin):
         user = User.objects.get(username='tester')
         self.assertEqual(user.profile.gender, ('male'))
 
-    def test_location(self):
+    def test_location_on_done(self):
         site = Site.objects.get(is_default_site=True)
         profile_settings = UserProfilesSettings.for_site(site)
 
@@ -785,7 +785,7 @@ class RegistrationDone(TestCase, MoloTestCaseMixin):
         user = User.objects.get(username='tester')
         self.assertEqual(user.profile.location, ('mlazi'))
 
-    def test_education_level(self):
+    def test_education_level_on_done(self):
         site = Site.objects.get(is_default_site=True)
         profile_settings = UserProfilesSettings.for_site(site)
 
