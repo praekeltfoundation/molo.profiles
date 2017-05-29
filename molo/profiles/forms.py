b246f6a6d42e5700f58c7d62caa2fa690a731667
Milton Madanda <madandat@gmail.com>
2017-05-29 12:36:31 +0200

    ensure request is passed to authentication form

--- molo/profiles/forms.py
+++ molo/profiles/forms.py
@@ -4,7 +4,9 @@ import re
 
 from django import forms
 from django.forms.extras.widgets import SelectDateWidget
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
@@ -20,6 +22,27 @@ REGEX_PHONE = settings.REGEX_PHONE if hasattr(settings, 'REGEX_PHONE') else \
 
 REGEX_EMAIL = settings.REGEX_EMAIL if hasattr(settings, 'REGEX_PHONE') else \
     r'([\w\.-]+@[\w\.-]+)'
+
+
+class MoloAuthenticationForm(AuthenticationForm):
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            self.user_cache = authenticate(request=self.request,
+                                           username=username,
+                                           password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(
+                    self.error_messages['invalid_login'],
+                    code='invalid_login',
+                    params={'username': self.username_field.verbose_name},
+                )
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
+        return self.cleaned_data
 
 
 def get_validation_msg_fragment():
