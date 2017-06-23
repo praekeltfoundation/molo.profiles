c9002207c05809cfb0fc758e21fcd6e255ee4b87
Jamie Hewland <jhewland@gmail.com>
2017-06-23 14:33:41 +0200

    Packaging updates
    * All the changes like in praekelt/molo#378

--- setup.py
+++ setup.py
@@ -1,39 +1,47 @@
+import codecs
 import os
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.rst')) as f:
-    readme = f.read()
+HERE = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'requirements.txt')) as f:
-    requires = filter(None, f.readlines())
 
-with open(os.path.join(here, 'requirements-dev.txt')) as f:
-    requires_dev = filter(None, f.readlines())
+def read(*parts):
+    with codecs.open(os.path.join(HERE, *parts), 'rb', 'utf-8') as f:
+        return f.read()
 
-with open(os.path.join(here, 'VERSION')) as f:
-    version = f.read().strip()
 
-setup(name='molo.profiles',
-      version=version,
-      description=('User profiles to be used with Molo.'),
-      long_description=readme,
-      classifiers=[
-          "Programming Language :: Python",
-          "Framework :: Django",
-          "Topic :: Internet :: WWW/HTTP",
-          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-      ],
-      author='Praekelt Foundation',
-      author_email='dev@praekelt.com',
-      url='http://github.com/praekelt/molo.profiles',
-      license='BSD',
-      keywords='praekelt, mobi, web, django',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      namespace_packages=['molo'],
-      install_requires=requires,
-      tests_require=requires_dev,
-      entry_points={})
+install_requires = [
+    'celery<4.0',
+    'django-daterange-filter',
+    'django-import-export',
+    'django-phonenumber-field',
+    'molo.core<6.0.0,>=5.0.0',
+]
+
+setup(
+    name='molo.profiles',
+    version=read('VERSION'),
+    description='User profiles to be used with Molo.',
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='Praekelt.org',
+    author_email='dev@praekelt.org',
+    url='http://github.com/praekelt/molo.profiles',
+    license='BSD-3-Clause',
+    keywords='praekelt, mobi, web, django',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    namespace_packages=['molo'],
+    install_requires=install_requires,
+)
